1e4458e5aa0f2826ed513fd3a7012191df3870f1
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2022-09-06 18:58:26 +0200

    92. Step 05 - Exploring Path Params and Path Variables with Spring Boot

--- src/main/java/com/in28minutes/springboot/firstrestapi/helloworld/HelloWorldResource.java
+++ src/main/java/com/in28minutes/springboot/firstrestapi/helloworld/HelloWorldResource.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.firstrestapi.helloworld;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,18 @@ public class HelloWorldResource {
 	}
 
 	@RequestMapping("/hello-world-bean")
-	//@ResponseBody
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+
+	@RequestMapping("/hello-world-path-param/{name}")
+	public HelloWorldBean helloWorldPathParam(@PathVariable String name) {
+		return new HelloWorldBean("Hello World, " + name);
+	}
+
+	@RequestMapping("/hello-world-path-param/{name}/message/{message}")
+	public HelloWorldBean helloWorldPathParam(@PathVariable String name, @PathVariable String message) {
+		return new HelloWorldBean("Hello World, " + name + ", " + message);
 	}
 
 }
