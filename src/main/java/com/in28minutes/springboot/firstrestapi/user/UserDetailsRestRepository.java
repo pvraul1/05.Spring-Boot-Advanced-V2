dd8079ad436926cb7a2e007f4f3a03f68729506d
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2022-09-10 10:02:20 +0200

    106. Step 17 - Creating User REST API with Spring Boot Starter Rest

---
+++ src/main/java/com/in28minutes/springboot/firstrestapi/user/UserDetailsRestRepository.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.firstrestapi.user;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface UserDetailsRestRepository extends PagingAndSortingRepository<UserDetails, Long> {
+
+	List<UserDetails> findByRole(String role);
+
+}
