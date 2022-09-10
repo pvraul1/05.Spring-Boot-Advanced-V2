82f8412ca0bfb342d933c92ff056a85a0fb0561d
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2022-09-10 18:34:02 +0200

    111. Step 21 - Writting Spring Boot Integration Test for GET method...

--- src/test/java/com/in28minutes/springboot/firstrestapi/survey/SurveyResourceIT.java
+++ src/test/java/com/in28minutes/springboot/firstrestapi/survey/SurveyResourceIT.java
@@ -17,6 +17,8 @@ class SurveyResourceIT {
 
 	private static final String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
 
+	private static final String GENERIC_QUESTION_URL = "/surveys/Survey1/questions";
+
 	@Autowired
 	TestRestTemplate template;
 
@@ -58,6 +60,31 @@ class SurveyResourceIT {
 		JSONAssert.assertEquals(str, responseEntity.getBody(), true);
 		JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false); // It have not options
 
+	}
+
+	@Test
+	void retrieveAllSurveyQuestions_basicScenario() throws JSONException {
+		ResponseEntity<String> responseEntity = template.getForEntity(GENERIC_QUESTION_URL, String.class);
+
+		String expectedResponse = 
+			"""
+				[
+					{
+						"id":"Question1"
+					},
+					{
+						"id":"Question2"
+					},
+					{
+						"id":"Question3"
+					}
+				]
+			""";
+
+		assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+		assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+
+		JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
 	}
 
 }
