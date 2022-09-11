a2388f3aeee496c61c3a71374893e2767fa225a3
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2022-09-11 14:08:02 +0200

    115. Step 25 - Improving Asserts for SB Mock MVC Unit Test

--- src/test/java/com/in28minutes/springboot/firstrestapi/survey/SurveyResourceTest.java
+++ src/test/java/com/in28minutes/springboot/firstrestapi/survey/SurveyResourceTest.java
@@ -1,6 +1,12 @@
 package com.in28minutes.springboot.firstrestapi.survey;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,6 +21,8 @@ class SurveyResourceTest {
 
 	private static final String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
 
+	private static final String GOOGLE_CLOUD = "Google Cloud";
+
 	@MockBean
 	private SurveyService surveyService;
 
@@ -22,13 +30,36 @@ class SurveyResourceTest {
 	private MockMvc mockMvc;
 
 	@Test
-	void tretrieveSpecificSurveyQuestionsest_basicScenario() throws Exception {
+	void tretrieveSpecificSurveyQuestionsest_404Scenario() throws Exception {
 		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(SPECIFIC_QUESTION_URL).accept(MediaType.APPLICATION_JSON);
 
 		MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
 
-		System.out.println(mvcResult.getResponse().getContentAsString());
-		System.out.println(mvcResult.getResponse().getStatus());
+		assertEquals(404, mvcResult.getResponse().getStatus());
+	}
+
+	@Test
+	void tretrieveSpecificSurveyQuestionsest_basicScenario() throws Exception {
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(SPECIFIC_QUESTION_URL).accept(MediaType.APPLICATION_JSON);
+
+		Question question = new Question("Question1", "Most Popular Cloud Platform Today",
+				Arrays.asList("AWS", "Azure", GOOGLE_CLOUD, "Oracle Cloud"), "AWS");
+
+		when(surveyService.retrieveSpecificSurveyQuestions("Survey1", "Question1")).thenReturn(question);
+
+		MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+
+		String expectedResponse = """
+				{
+					"id":"Question1",
+					"description":"Most Popular Cloud Platform Today",
+					"options":["AWS","Azure","Google Cloud","Oracle Cloud"],
+					"correctAnswer":"AWS"
+				}
+				""";
+
+		assertEquals(200, mvcResult.getResponse().getStatus());
+		JSONAssert.assertEquals(expectedResponse, mvcResult.getResponse().getContentAsString(), false);
 	}
 
 }
